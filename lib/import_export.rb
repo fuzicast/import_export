31f5249eeacb24998bc0f828889165ac232a0580
Yue Jiang <yuejiang@Yues-MacBook-Pro-2.local>
2014-12-09 22:59:47 -0500

    Initial commit

---
+++ lib/import_export.rb
@@ -0,0 +1,5 @@
+require "import_export/version"
+
+module ImportExport
+  # Your code goes here...
+end
---
+++ lib/item.rb
@@ -0,0 +1,13 @@
+class Item
+  attr_accessor :id, :desc, :price, :cost, :price_type, :quantity_on_hand, :modifiers
+
+  def initialize(*args)
+    @id = args[0]
+    @id = args[0]
+    @id = args[0]
+    @id = args[0]
+    @id = args[0]
+    @id = args[0]
+    @id = args[0]
+  end
+end
